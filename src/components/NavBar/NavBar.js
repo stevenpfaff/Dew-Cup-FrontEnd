840195a338f189365c436787b7686cfdfc036693
stevenpfaff <pfaffy1426@gmail.com>
2021-11-12 14:33:57 -0600

    added styling to navbar as well as dew cup logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,26 +2,27 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-
+import DewCup from "../../Images/DewCup.png"
 
 
 
 function NavBar(props) {
     return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
   <Container>
         <a class="navbar-brand" href="#">
         </a>
+        <img src={DewCup} alt="..." height="75" width="75"></img>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/Teams">Teams</Nav.Link>  
-                    {props.user &&<Nav.Link href="/CreateTeam"> Create Team </Nav.Link>}
-                    <Nav.Link href="/Players">Players</Nav.Link>
-                    {props.user &&<Nav.Link href="/CreatePlayer">Create Player</Nav.Link>}                 
+                    <Nav.Link href="/Teams">Teams</Nav.Link> 
+                    <Nav.Link href="/Players">Players</Nav.Link>                 
                     </Nav>
                     <Nav>
+                    {props.user &&<Nav.Link href="/CreatePlayer">Create Player</Nav.Link>}
+                    {props.user &&<Nav.Link href="/CreateTeam"> Create Team </Nav.Link>}
                         {!props.user && <Nav.Link onClick={props.userSignIn} href="/login">Login</Nav.Link>}
                     {!props.user &&<Nav.Link  onClick={props.createNewUser} href="/register">Register</Nav.Link>} 
                     {props.user &&<Nav.Link onClick={props.logOutUser} href="/login">Logout</Nav.Link>}
