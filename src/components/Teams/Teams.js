77ada819a642c61801f36995fad09aaefed46116
stevenpfaff <pfaffy1426@gmail.com>
2021-12-10 09:33:22 -0600

    added sort functions to teams page

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -4,6 +4,7 @@ import { Table } from 'react-bootstrap'
 import "./Teams.css"
 import { Link } from 'react-router-dom';
 import { SortAlphaUp } from 'react-bootstrap-icons'
+import { SortNumericDown } from 'react-bootstrap-icons'
 
 class Teams extends Component {
     constructor(props) {
@@ -52,6 +53,132 @@ class Teams extends Component {
         }))
     }
 
+    sortWins = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.wins < team2.wins) {
+                    return 1;
+                }
+                if (team1.wins > team2.wins) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortLosses = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.losses < team2.losses) {
+                    return 1;
+                }
+                if (team1.losses > team2.losses) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortGoals = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.goals < team2.goals) {
+                    return 1;
+                }
+                if (team1.goals > team2.goals) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortGoalsAg = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.goals_against < team2.goals_against) {
+                    return 1;
+                }
+                if (team1.goals_against > team2.goals_against) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortRuns = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.runs < team2.runs) {
+                    return 1;
+                }
+                if (team1.runs > team2.runs) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortRunsAg = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.runs_against < team2.runs_against) {
+                    return 1;
+                }
+                if (team1.runs_against > team2.runs_against) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortAverage = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.batting_average < team2.batting_average) {
+                    return 1;
+                }
+                if (team1.batting_average > team2.batting_average) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortHomeruns = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.homeruns < team2.homeruns) {
+                    return 1;
+                }
+                if (team1.homeruns > team2.homeruns) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
+    sortChampionships = () => {
+        this.setState(prevState => ({
+            players: [...prevState.teams].sort(function (team1, team2) {
+                if (team1.championships < team2.championships) {
+                    return 1;
+                }
+                if (team1.championships > team2.championships) {
+                    return -1;
+                }
+                return 0;
+            }),
+        }))
+    }
+
     render() {
         return (
             <div style={{ marginRight: "15%", marginLeft: "15%", marginBottom: "10%" }} >
@@ -61,15 +188,15 @@ class Teams extends Component {
                     <thead>
                         <tr>
                             <th>Team Name<button type="button" class="btn btn-default" onClick={this.sortTeamName}><SortAlphaUp /></button></th>
-                            <th>Wins</th>
-                            <th>Losses</th>
-                            <th>Goals</th>
-                            <th>Goals Against </th>
-                            <th>Runs</th>
-                            <th>Runs Against</th>
-                            <th>Batting Average</th>
-                            <th>Homeruns</th>
-                            <th>Championships</th>
+                            <th>Wins<button type="button" class="btn btn-default" onClick={this.sortWins}><SortNumericDown /></button></th>
+                            <th>Losses<button type="button" class="btn btn-default" onClick={this.sortLosses}><SortNumericDown /></button></th>
+                            <th>Goals<button type="button" class="btn btn-default" onClick={this.sortGoals}><SortNumericDown /></button></th>
+                            <th>Goals Against<button type="button" class="btn btn-default" onClick={this.sortGoalsAg}><SortNumericDown /></button></th>
+                            <th>Runs<button type="button" class="btn btn-default" onClick={this.sortRuns}><SortNumericDown /></button></th>
+                            <th>Runs Against<button type="button" class="btn btn-default" onClick={this.sortRunsAg}><SortNumericDown /></button></th>
+                            <th>Batting Average<button type="button" class="btn btn-default" onClick={this.sortAverage}><SortNumericDown /></button></th>
+                            <th>Homeruns<button type="button" class="btn btn-default" onClick={this.sortHomeruns}><SortNumericDown /></button></th>
+                            <th>Championships<button type="button" class="btn btn-default" onClick={this.sortChampionships}><SortNumericDown /></button></th>
                         </tr>
                     </thead>
                     <tbody>
