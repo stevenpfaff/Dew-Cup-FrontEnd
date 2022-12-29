2ad8faab2b878f6b1698bf91dcc1a88d30260122
stevenpfaff <pfaffy1426@gmail.com>
2022-12-29 15:46:57 -0600

    netlify deploy

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import PBS from "../../Images/PBS.png"
+
 
 
 
@@ -13,19 +13,6 @@ function NavBar() {
                 <img src={PBS} alt="..." height="75" width="75"></img>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Tournament Results</Nav.Link>
-                        <Nav.Link href="/Games">Game Results</Nav.Link>
-
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="/Teams">Team Stats</Nav.Link>
-                        <Nav.Link href="/hockeystats">Hockey Stats</Nav.Link>
-                        <Nav.Link href="/mbstats">Minibat Stats</Nav.Link>
-                    </Nav>
-
-                </Navbar.Collapse>
 
             </Container>
         </Navbar>
--- src/components/Tournament/Tournaments.js
+++ src/components/Tournament/Tournaments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
+import tourney from '../../data/tourneys.json'
+import Button from '@material-ui/core/Button';
 
 
 class Tourneys extends Component {
@@ -8,30 +10,32 @@ class Tourneys extends Component {
         this.state = { tourney: [] }
     }
 
-    refreshList() {
-        fetch(process.env.REACT_APP_API + 'tourney')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ tourney: data })
-            })
-    }
+    //refreshList() {
+    //fetch(process.env.REACT_APP_API + 'tourney')
+    //.then(response => response.json())
+    //.then(data => {
+    //this.setState({ tourney: data })
+    //})
+    //}
 
-    componentDidMount() {
-        this.refreshList();
-    }
+    //componentDidMount() {
+    //this.refreshList();
+    //}
 
-    componentDidUpdate() {
-        this.refreshList();
-    }
+    //componentDidUpdate() {
+    //this.refreshList();
+    //}
 
     render() {
-        const { tourney } = this.state;
-        const sorted = tourney.sort((a, b) => a.TourneyName > b.TourneyName ? 1 : -1)
+        //const { tourney } = this.state;
+        //const sorted = tourney.sort((a, b) => a.number > b.number ? 1 : -1)
 
         return (
             <div style={{ marginRight: "25%", marginLeft: "25%", marginBottom: "10%" }}>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <h1 style={{ marginRight: "10%", marginLeft: "10%", marginBottom: "5%", marginTop: "5%", fontFamily: "inherit" }}>Tournaments</h1>
+                <a target="_blank" href="https://docs.google.com/spreadsheets/d/1--Z4rwRCaDi32B6K8KxQHrcy5mvSjfYOzOZjyPbgK20/edit#gid=820517614"><Button type="submit" variant="contained" class="btn btn-primary">Minibat Stats And Results</Button></a>
+                <a target="_blank" href="https://docs.google.com/spreadsheets/d/1-S9klqwfLvodu_MMg5HCc9CZSUmf3a7aA6NT9dRb7ro/edit#gid=469599973"><Button type="submit" variant="contained" class="btn btn-primary">Pond Hockey Stats And Results</Button></a>
                 <Table striped bordered hover>
                     <thead>
                         <tr>
@@ -43,18 +47,18 @@ class Tourneys extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {sorted.map((trny) => (
-                            <tr key={trny.Id}>
-                                <td>{trny.TourneyName}</td>
-                                <td>{trny.Winner}</td>
-                                <td>{trny.RunnerUp}</td>
-                                <td>{trny.Players}</td>
+                        {tourney.map((trny) => (
+                            <tr>
+                                <td>{trny.tourney}</td>
+                                <td>{trny.winner}</td>
+                                <td>{trny.runnerup}</td>
+                                <td>{trny.players}</td>
                                 <td>{trny.MVP}</td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
-            </div>
+            </div >
         );
     }
 }
