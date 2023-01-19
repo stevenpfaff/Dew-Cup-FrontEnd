4cef0b487c3ddeaea29da81270880127690053ce
stevenpfaff <pfaffy1426@gmail.com>
2023-01-18 19:33:38 -0600

    updated player stats

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import team from '../../data/teams.json';
 
 class Teams extends Component {
     constructor(props) {
@@ -9,54 +10,59 @@ class Teams extends Component {
         this.state = { team: [] }
     }
 
-    refreshList() {
-        fetch(process.env.REACT_APP_API + 'team')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ team: data })
-            })
-    }
+    // refreshList() {
+    //     fetch(process.env.REACT_APP_API + 'team')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             this.setState({ team: data })
+    //         })
+    // }
 
-    componentDidMount() {
-        this.refreshList();
-    }
+    // componentDidMount() {
+    //     this.refreshList();
+    // }
 
-    componentDidUpdate() {
-        this.refreshList();
-    }
+    // componentDidUpdate() {
+    //     this.refreshList();
+    // }
 
     render() {
-        const { team } = this.state;
-        const sortedTitle = team.sort((a, b) => b.Championships > a.Championships ? 1 : -1)
+        // const { team } = this.state;
+        // const sortedTitle = team.sort((a, b) => b.Championships > a.Championships ? 1 : -1)
         return (
             <div style={{ marginRight: "25%", marginLeft: "25%", marginBottom: "10%" }} >
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <h1 style={{ marginRight: "10%", marginLeft: "10%", marginBottom: "5%", marginTop: "5%", fontFamily: "inherit" }}>All-Time Team Stats</h1>
-                <Link to={'/coorsclash'}><Button type="submit" variant="contained" class="btn btn-dark">Coors Clash Standings</Button></Link>
                 <Table striped bordered hover>
                     <thead>
                         <tr>
                             <th>Name</th>
-                            <th>W</th>
-                            <th>L</th>
+                            <th>Hockey W</th>
+                            <th>Hockey L</th>
                             <th>G</th>
                             <th>GA</th>
+                            <th>Bats W</th>
+                            <th>Bats L</th>
                             <th>R</th>
                             <th>RA</th>
-                            <th>Titles</th>
+                            <th>Hockey Titles</th>
+                            <th>Bats Titles</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {sortedTitle.map((team) => (
+                        {team.map((team) => (
                             <tr key={team.Id}>
-                                <td>{team.TeamName}</td>
-                                <td>{team.Wins}</td>
-                                <td>{team.Losses}</td>
-                                <td>{team.Goals}</td>
-                                <td>{team.GoalsAg}</td>
-                                <td>{team.Runs}</td>
-                                <td>{team.RunsAg}</td>
-                                <td>{team.Championships}</td>
+                                <td>{team.name}</td>
+                                <td>{team.hockeywins}</td>
+                                <td>{team.hockeylosses}</td>
+                                <td>{team.goals}</td>
+                                <td>{team.goalsag}</td>
+                                <td>{team.batswins}</td>
+                                <td>{team.batslosses}</td>
+                                <td>{team.runs}</td>
+                                <td>{team.runsag}</td>
+                                <td>{team.hockeychampionships}</td>
+                                <td>{team.batschampionships}</td>
                             </tr>
                         ))}
                     </tbody>
